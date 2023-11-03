77b72813c477ea524465fe4e894d9871c3ec2d8c
One <43485962+c-git@users.noreply.github.com>
2023-11-02 22:48:15 -0400

    Add lints and remove test code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![cfg_attr(test, deny(warnings))]
+#![forbid(unsafe_code)]
+#![doc = include_str!("../README.md")]
